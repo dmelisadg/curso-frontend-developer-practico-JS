145ccbf6f1ccf98e0cbc2e969a2c7a6f67e3037c
dmelisadg <dmdoming@gmail.com>
2023-05-04 09:00:59 -0500

    Re-organizing Product detail styles

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const cardsContainer = document.querySelector('.cards-container');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -12,17 +13,17 @@ menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
     if (!isAsideClosed) {
-        aside.classList.add('inactive')
+        shoppingCartContainer.classList.add('inactive')
     }
     desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
     if (!isAsideClosed) {
-        aside.classList.add('inactive')
+        shoppingCartContainer.classList.add('inactive')
     }
     mobileMenu.classList.toggle('inactive')
 }
@@ -38,7 +39,69 @@ function toggleCarritoAside() {
         desktopMenu.classList.add('inactive')
     }
 
-    aside.classList.toggle('inactive')
+    shoppingCartContainer.classList.toggle('inactive')
 }
+
+function renderProducts(arr){
+    
+    for (product of productList){
+        const productCard = document. createElement('div');
+        productCard.classList.add('product-card');
+   
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image)
+   
+        const productInfo = document. createElement('div');
+        productInfo.classList.add('product-info');
+   
+        const productInfoDiv = document. createElement('div');
+   
+        const productPrice = document. createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document. createElement('p');
+        productName.innerText = product.name;
+   
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+   
+        const productInfoFigure = document. createElement('figure');
+        const productImgCart = document. createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+   
+        productInfoFigure.appendChild(productImgCart);
+   
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+   
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+   
+        cardsContainer.appendChild(productCard);
+   
+        
+   }
+}
+
+const productList = [];
+productList.push({
+    name:'bicicleta',
+    price: '1200',
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+});
+productList.push({
+    name:'Triciclo',
+    price: '1500',
+    image: "https://images.pexels.com/photos/10513693/pexels-photo-10513693.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+});
+productList.push({
+    name:'monopatin',
+    price: '2200',
+    image: "https://images.pexels.com/photos/2727413/pexels-photo-2727413.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+});
+
+renderProducts(productList);
+
+
+
 
 
